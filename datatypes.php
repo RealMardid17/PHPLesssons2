1bb4034fe047cd9b4f644e67d5b37707356aee42
RealMardid17 <141748561+RealMardid17@users.noreply.github.com>
2025-03-28 09:05:29 -0500

    new

---
+++ datatypes.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+
+  <body>
+  
+<?php   
+  //string 
+  $name = 'Coding is fun';
+
+  //Interger
+  $name = 20;
+
+  //Float
+  $name = 20.5784
+
+  //BooLean
+  true = 1
+  false = 0
+
+  //Array
+    $names = array("Daniel","Dennis","Michael");
+    echo $names['1'];
+
+?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+ 
+</body>
+
+</html>
